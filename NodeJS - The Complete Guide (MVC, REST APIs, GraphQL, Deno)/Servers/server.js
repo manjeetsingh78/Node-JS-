9bc062b79cd4293231c84f7482ec34d1870ea1d4
Manjeet Singh <rambaghel741791@gmail.com>
2026-08-07 22:55:36 +0530

    I have played with req and res

---
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/server.js
@@ -0,0 +1,33 @@
+const http = require("http");
+
+// function rqListener(req,res) {
+
+// }
+
+// createServer creates a server and returns a null value
+
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head>My first page</head>');
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end();
+    }
+    //console.log(req);
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head>My first page</head>');
+    res.write('<body>Hello from Nodejs Server</body>')
+    res.write('</html>');
+    res.end();
+    // res.write(); it will send error because it is used after res.end();
+    //process.exit(); // It gets it out of event loop which runs forever
+});
+
+// listen is used to expose port on machine 
+server.listen(3000);
