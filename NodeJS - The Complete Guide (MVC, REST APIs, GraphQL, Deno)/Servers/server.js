0ed217db59c1ecaafd237ea22b6391d99043e9b6
Manjeet Singh <rambaghel741791@gmail.com>
2026-08-07 23:18:32 +0530

    Parsing Request bodies

--- NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/server.js
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/server.js
@@ -10,31 +10,54 @@ const fs = require("fs");
 const server = http.createServer((req,res)=>{
     const url = req.url;
     const method = req.method;
+
     if(url === '/'){
         res.write('<html>');
         res.write('<head>My first page</head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
         res.write('</html>');
         return res.end();
     }
 
     if(url === '/message' && method === 'POST'){
+
+        const body=[];
+
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+            console.log(parsedBody);
+        });
+
         fs.writeFileSync('message.txt','DUMMY');
+
         res.statusCode = 302;
+
         res.setHeader('Location','/');
+
         return res.end();
     }
 
     //console.log(req);
-    console.log(req.url);
-    console.log(req.method);
-    console.log(req.headers);
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head>My first page</head>');
-    res.write('<body>Hello from Nodejs Server</body>')
-    res.write('</html>');
-    res.end();
+    // console.log(req.url);
+
+    // console.log(req.method);
+
+    // console.log(req.headers);
+
+    // res.setHeader('Content-Type','text/html');
+
+    // res.write('<html>');
+    // res.write('<head>My first page</head>');
+    // res.write('<body>Hello from Nodejs Server</body>')
+    // res.write('</html>');
+    // res.end();
     // res.write(); it will send error because it is used after res.end();
     //process.exit(); // It gets it out of event loop which runs forever
 });
