f391f37f2027caae889f4af0adef032b00436155
Manjeet Singh <rambaghel741791@gmail.com>
2026-08-08 23:12:30 +0530

    There has been added a important note in the code

--- NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
@@ -1,19 +1,44 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-// it creates a middleware
-app.use((req, res, next) => {
-    console.log("in the middleware");
-    next(); // it allows requests to next middleware in the line
-});
 
-app.use((req, res, next) => {
+// Note:  Express checks middleware from top to bottom.
+
+/*
+
+Why /path is before /?
+
+Because / is a very broad path.
+
+app.use("/", ...) matches almost every URL, including:
+/
+ /path
+ /hello
+ /abc
+ /path/test
+Whereas:
+
+app.use("/path", ...)
+
+matches URLs beginning with /path, such as:
+
+/path
+/path/test
+/path/anything
+
+*/
+
+app.use("/path",(req, res, next) => {
     console.log("in another middleware");
+    res.send("<h1>Hello from Path endpoint</h1>");
 });
 
-const server = http.createServer(app);
+app.use("/",(req, res, next) => {
+    console.log("in another middleware");
+    res.send("<h1>Hello from Express</h1>");
+});
 
-server.listen(3000);
+//const server = http.createServer(app);
+
+app.listen(3000);
