4ae5c0c78847554d05110e01f0c74d175233e477
Manjeet Singh <rambaghel741791@gmail.com>
2026-08-09 15:00:38 +0530

    Styling has been added in the pages

--- NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 
 app.use(shopRoutes);
 //const server = http.createServer(app);
