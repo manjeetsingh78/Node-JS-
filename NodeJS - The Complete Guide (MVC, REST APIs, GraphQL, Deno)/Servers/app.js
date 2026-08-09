4eea130f1c2ee0488f2055a0ffcd383757370e13
Manjeet Singh <rambaghel741791@gmail.com>
2026-08-09 09:42:29 +0530

    Seen usage of body-parser on Incoming Requests

--- NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
 
 // Note:  Express checks middleware from top to bottom.
 
@@ -29,14 +31,18 @@ matches URLs beginning with /path, such as:
 
 */
 
-app.use("/path",(req, res, next) => {
-    console.log("in another middleware");
-    res.send("<h1>Hello from Path endpoint</h1>");
+
+app.use("/add-product",(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>');
+});
+
+app.use("/product",(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use("/",(req, res, next) => {
-    console.log("in another middleware");
-    res.send("<h1>Hello from Express</h1>");
+    res.send('<h1>Hello from express</h1>');
 });
 
 //const server = http.createServer(app);
