a6d56df4e55e259e0167076488496a1cf432c193
Manjeet Singh <rambaghel741791@gmail.com>
2026-08-09 10:43:16 +0530

    Serving HTML Pages using router and path module

--- NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -7,13 +8,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 
 app.use(shopRoutes);
 //const server = http.createServer(app);
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname,"views","404.html"));
 });
 
 app.listen(3000);
