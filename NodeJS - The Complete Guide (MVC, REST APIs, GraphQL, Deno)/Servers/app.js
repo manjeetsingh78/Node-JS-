11e4832b7fa263f574a17d2b00177414328c38c2
Manjeet Singh <rambaghel741791@gmail.com>
2026-08-10 22:13:44 +0530

    Converting Html files to pug files

--- NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/Servers/app.js
@@ -6,6 +6,8 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine','pug');
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,7 +17,7 @@ app.use(shopRoutes);
 //const server = http.createServer(app);
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname,"views","404.html"));
+    res.status(404).render('404');
 });
 
 app.listen(3000);
